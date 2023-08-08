0acea05270ddd72df00da2059077feff0a813470
dmarkox <markodave46@gmail.com>
2023-08-09 02:58:54 +0300

    unami

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -24,7 +24,7 @@ const siteMetadata = {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    umamiWebsiteId: '2f4dc5c8-146c-4ec1-a730-e0f6fca850b9', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
