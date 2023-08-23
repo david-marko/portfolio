ec10ff2b32e665da7faa76af4c75264143816a46
dmarkox <markodave46@gmail.com>
2023-08-23 11:40:59 +0300

    added google tracking id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import '@fontsource/open-sans/500.css';
 import '@fontsource/open-sans/600.css';
 import '@fontsource/open-sans/700.css';
 import '@fontsource/open-sans/800.css';
+import * as gtag from 'pages/gtag';
 
 import { ThemeProvider, useTheme } from 'next-themes';
 import type { AppProps } from 'next/app';
@@ -22,11 +23,25 @@ import LayoutWrapper from '@/components/LayoutWrapper';
 import siteMetadata from '@/data/siteMetadata';
 
 import { GeistProvider } from '@geist-ui/core';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import Script from 'next/script';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 const isSocket = process.env.SOCKET;
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider attribute='class' defaultTheme={siteMetadata.theme}>
       <Head>
@@ -39,6 +54,24 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </LayoutWrapper>
       </GeistProviderWithTheme>
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </ThemeProvider>
   );
 }
